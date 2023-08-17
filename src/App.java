9cbef28bfb0e0877417ca0c425942268f88007f5
Daniel52Alejandro <a01643741@tec.mx>
2023-08-16 18:54:57 -0600

    Mis canciones favoritas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Trauma");
+        System.out.println("Haha, hi");
+        System.out.println("prairies");
+        System.out.println("say you wont let go");
+        System.out.println("what up danger");
+        System.out.println("riptide");
     }
 }
